68f77f7d0e33701e308a95ce94d60e0584c97863
Tristan-Ha <tristanha02@gmail.com>
2020-09-12 21:16:49 -0700

    changed wording in about section and edited header animation to travel farther

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 function animate() {
   var elem = document.getElementById('header-text');
-  elem.style.left = "2000px";
+  elem.style.left = "2500px";
   var start = Date.now();
   var timer = setInterval(function() {
     var timePassed = Date.now() - start;
-    elem.style.left = parseInt(elem.style.left) - 20 + 'px';
+    elem.style.left = parseInt(elem.style.left) - 25 + 'px';
     if (timePassed >= 500) {
       clearInterval(timer);
       return;
