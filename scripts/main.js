7a6cf5154a42491f6976a2979745ce769f141c2c
Tristan-Ha <tristanha02@gmail.com>
2020-09-14 13:33:27 -0700

    changed animation to rely on position rather than time

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 function animate() {
   var elem = document.getElementById('header-text');
   elem.style.left = "2500px";
-  var start = Date.now();
   var timer = setInterval(function() {
-    var timePassed = Date.now() - start;
     elem.style.left = parseInt(elem.style.left) - 25 + 'px';
-    if (timePassed >= 500) {
+    if (elem.style.left == "0px") {
       clearInterval(timer);
       return;
     }
